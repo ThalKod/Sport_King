05ef811f07bb2e32bbfd5259410cd0e9a5ffde6c
Thal <tmarcelin@thal.tech>
2022-01-15 14:40:54 +0100

    add appollo

---
+++ config.js
@@ -0,0 +1,13 @@
+import { Platform } from 'react-native';
+import Config from 'react-native-config';
+
+export default {
+  API_HOST: Platform.select({
+    ios: Config.IOS_API_HOST,
+    android: Config.ANDROID_API_HOST,
+  }),
+  REWARDED_ADS: Platform.select({
+    ios: Config.REWARDED_ADS_ANDROID,
+    android: Config.REWARDED_ADS_IOS,
+  })
+};
--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,10 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig } = require("metro-config");
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+exports.resolver = {
+  ...defaultResolver,
+  sourceExts: [
+    ...defaultResolver.sourceExts,
+    "cjs",
+  ],
 };
