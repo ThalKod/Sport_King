8d9149f63971b81553030127a00e19336af756ee
Thal <tmarcelin@thal.tech>
2022-01-19 16:29:18 +0100

    add routes folder

--- App.js
+++ App.js
@@ -13,7 +13,44 @@ const App: () => Node = () => {
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
-
+        <Drawer.Navigator
+          initialRouteName="Paryaj Sport"
+          drawerContent={(props) => ( <CustomDrawerContent {...props} />)}
+          drawerStyle={{
+            backgroundColor: '#261D44',
+          }}
+          drawerContentOptions={{
+            inactiveTintColor: '#fff',
+          }}
+        >
+          <Drawer.Screen
+            name="Paryaj Sport"
+            component={rootDrawerNavigator}
+            options={{
+              drawerIcon: ({focused, size}) => (
+                <FontAwesome5Icons name="dice" size={moderateScale(14)} color="#fff" />
+              )
+            }}
+          />
+          {/* <Drawer.Screen
+                  name="Rezilta Live"
+                  component={liveScoreDrawerNavigation}
+                  options={{
+                    drawerIcon: ({focused, size}) => (
+                        <AntDesignIcons name="videocamera" size={moderateScale(18)} color="#fff" />
+                    )
+                  }}
+              /> */}
+          <Drawer.Screen
+            name="Live Sports"
+            component={liveTvDrawerNavigation}
+            options={{
+              drawerIcon: ({focused, size}) => (
+                <MaterialIcons name="live-tv" size={moderateScale(18)} color="#fff" />
+              )
+            }}
+          />
+        </Drawer.Navigator>
       </NavigationContainer>
     </ApolloProvider>
   );
---
+++ src/routes/utils/getHeaderTitle.js
@@ -0,0 +1,20 @@
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+
+export function getHeaderTitle(route) {
+  // If the focused route is not found, we need to assume it's the initial screen
+  // This can happen during if there hasn't been any navigation inside the screen
+  // In our case, it's "Feed" as that's the first screen inside the navigator
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Feed';
+
+  switch (routeName) {
+    case 'Acceuil':
+      return 'Acceuil';
+    case 'Matchs':
+      return 'Matchs';
+    case 'Paris':
+      return 'Paris';
+    case 'Classement':
+      return "Classement"
+  }
+}
