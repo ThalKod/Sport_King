718dab0f489a4758423315ba8324dc1ab037cbb4
Thal <tmarcelin@thal.tech>
2022-05-07 14:53:52 +0200

    add navigation bottom drawer

--- android/app/src/main/java/com/sport_king/MainActivity.java
+++ android/app/src/main/java/com/sport_king/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Sport_King";
+    return "Sport King";
   }
 
   @Override
